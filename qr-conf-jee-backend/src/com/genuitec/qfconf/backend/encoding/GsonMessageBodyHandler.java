a545cd74a0a01ca7b066eb20730b46a88d5bbf45
Timothy Webb <tim@genuitec.com>
2014-02-10 08:18:54 -0600

    Clean up Gson encoding

--- qr-conf-jee-backend/src/com/genuitec/qfconf/backend/encoding/GsonMessageBodyHandler.java
+++ qr-conf-jee-backend/src/com/genuitec/qfconf/backend/encoding/GsonMessageBodyHandler.java
@@ -26,7 +26,7 @@ import com.google.gson.GsonBuilder;
 public final class GsonMessageBodyHandler implements MessageBodyWriter<Object>,
 		MessageBodyReader<Object> {
 
-	private static final String UTF_8 = "UTF-8";
+	private static final String UTF8 = "UTF-8";
 
 	private Gson gson;
 
@@ -39,18 +39,12 @@ public final class GsonMessageBodyHandler implements MessageBodyWriter<Object>,
 	}
 
 	@Override
-	public boolean isReadable(Class<?> type, Type genericType,
-			java.lang.annotation.Annotation[] annotations, MediaType mediaType) {
-		return true;
-	}
-
-	@Override
 	public Object readFrom(Class<Object> type, Type genericType,
 			Annotation[] annotations, MediaType mediaType,
 			MultivaluedMap<String, String> httpHeaders, InputStream entityStream)
 			throws IOException, WebApplicationException {
 		InputStreamReader streamReader = new InputStreamReader(entityStream,
-				UTF_8);
+				UTF8);
 		try {
 			Type jsonType;
 			if (type.equals(genericType)) {
@@ -65,24 +59,12 @@ public final class GsonMessageBodyHandler implements MessageBodyWriter<Object>,
 	}
 
 	@Override
-	public boolean isWriteable(Class<?> type, Type genericType,
-			Annotation[] annotations, MediaType mediaType) {
-		return true;
-	}
-
-	@Override
-	public long getSize(Object object, Class<?> type, Type genericType,
-			Annotation[] annotations, MediaType mediaType) {
-		return -1;
-	}
-
-	@Override
 	public void writeTo(Object object, Class<?> type, Type genericType,
 			Annotation[] annotations, MediaType mediaType,
 			MultivaluedMap<String, Object> httpHeaders,
 			OutputStream entityStream) throws IOException,
 			WebApplicationException {
-		OutputStreamWriter writer = new OutputStreamWriter(entityStream, UTF_8);
+		OutputStreamWriter writer = new OutputStreamWriter(entityStream, UTF8);
 		try {
 			Type jsonType;
 			if (type.equals(genericType)) {
@@ -94,5 +76,23 @@ public final class GsonMessageBodyHandler implements MessageBodyWriter<Object>,
 		} finally {
 			writer.close();
 		}
+	}
+
+	@Override
+	public boolean isReadable(Class<?> type, Type genericType,
+			java.lang.annotation.Annotation[] annotations, MediaType mediaType) {
+		return true;
+	}
+
+	@Override
+	public boolean isWriteable(Class<?> type, Type genericType,
+			Annotation[] annotations, MediaType mediaType) {
+		return true;
+	}
+
+	@Override
+	public long getSize(Object object, Class<?> type, Type genericType,
+			Annotation[] annotations, MediaType mediaType) {
+		return -1;
 	}
 }
