d3c95ada1a44fbc32c8d7575cf1b5f74a763eea6
Igor <igorizr1@gmail.com>
2014-02-13 17:02:49 +0200

    other qr

--- www/app/viewHelpers/scannow_page.js
+++ www/app/viewHelpers/scannow_page.js
@@ -1,17 +1,30 @@
 (function(View, Router){
     
     View.scannow_page = function(data){
-        $('#scanner .scan-inner').html5_qrcode(function(data){
-            console.log(data);
-            // do something when code is read
-        },
-        function(error){
-            alert(error);
-            //show read errors 
-        }, function(videoError){
-            alert(videoError);
-            //the video stream could be opened
-        });
+//        $('#scanner .scan-inner').html5_qrcode(function(data){
+//            console.log(data);
+//            // do something when code is read
+//        },
+//        function(error){
+//            alert(error);
+//            //show read errors 
+//        }, function(videoError){
+//            alert(videoError);
+//            //the video stream could be opened
+//        });
+
+
+        cordova.plugins.barcodeScanner.scan(
+            function (result) {
+                alert("We got a barcode\n" +
+                      "Result: " + result.text + "\n" +
+                      "Format: " + result.format + "\n" +
+                      "Cancelled: " + result.cancelled);
+            }, 
+            function (error) {
+                alert("Scanning failed: " + error);
+            }
+         );
     };
     
 }(App.viewHelpers, App.Router));
