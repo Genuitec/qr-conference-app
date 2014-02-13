87f8ad5dfb79749382357a97af8019044c011b20
Igor <igorizr1@gmail.com>
2014-02-13 16:05:58 +0200

    features

--- www/app/viewHelpers/scannow_page.js
+++ www/app/viewHelpers/scannow_page.js
@@ -2,11 +2,14 @@
     
     View.scannow_page = function(data){
         $('#scanner .scan-inner').html5_qrcode(function(data){
+            console.log(data);
             // do something when code is read
         },
         function(error){
+            alert(error);
             //show read errors 
         }, function(videoError){
+            alert(videoError);
             //the video stream could be opened
         });
     };
