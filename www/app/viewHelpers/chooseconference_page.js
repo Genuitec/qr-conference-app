e22bc611c45bc9a844ddaa67c87165469083b143
Igor <igorizr1@gmail.com>
2014-03-11 13:45:26 +0200

    tags bug fixed...and error in myhelpers fixed..also bgdync modified...also added the time comparation for updatetime

--- www/app/viewHelpers/chooseconference_page.js
+++ www/app/viewHelpers/chooseconference_page.js
@@ -22,15 +22,16 @@
     
     template;
     
-//    $(document).ready(function(){
-//        $("#chooseconference_page .refresh").ham("click", function(){
-//            Sync(true, function(){
-//                Conference.read(function(d){
-//                    $("#chooseconference_page .main").html(template(d));
-//                });
-//            });
-//        });
-//    });
+    $(document).ready(function(){
+        $("#chooseconference_page .refresh").ham("click", function(){
+            if( ( (Math.floor( ( ( (new Date()).getTime() + parseInt(Session.get("serverTimeDifference"),10) ) - parseInt(Session.get("lastSync"),10) ) /1000 /60 ) ) > 0) )
+                Sync(true, function(){
+                    Conference.read(function(d){
+                        $("#chooseconference_page .main").html(template(d));
+                    });
+                });
+        });
+    });
     
     View.chooseconference_page = function(data){
         template = Handlebars.compile($("#chooseconference_page-template").html());
