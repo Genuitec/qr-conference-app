85257b9a264b01bfacde5c7544313c4e38740060
Igor <igorizr1@gmail.com>
2014-03-28 11:20:37 +0200

    back tag

--- www/app/models/Tag_model.js
+++ www/app/models/Tag_model.js
@@ -1,6 +1,39 @@
-(function(Models, DB){
+(function(Models, DB, Session, getConfig){
     
     Models.Tag = {
+        
+        create : function(data, callback){},
+                
+        read : function(where, callback){
+            if(empty(where) || empty(where.conference_id))callback(false);
+            DB.select("tags");
+            DB.from("scans");
+            DB.where('conference_id = "'+ where.conference_id +'"');
+            if(is_set(where.scan_id))
+                DB.where('id = "'+ where.scan_id+'"');
+            DB.col(function(data){
+                callback( empty(data) ? [] : data.split(",") );
+            });
+        },
+        conferenceTags : function(where, callback){
+            if(empty(where) || empty(where.conference_id)) return false;
+            DB.select("tags");
+            DB.from("conferences");
+            DB.where('id = "'+ where.conference_id +'"');
+            DB.col(function(data){
+                callback( empty(data) ? [] : data.split(",") );
+            });
+        },
+                
+        available : function(where, callback){
+            if(empty(where) || empty(where.scan_id) || empty(where.conference_id))callback(false);
+            DB.select("tags");
+            DB.from("conferences");
+            DB.where('id = '+ where.conference_id);
+            DB.col(function(data){
+                callback( empty(data) ? [] : data.split(",") );
+            });
+        },
                 
         update : function(data, where, callback){
             DB.update("scans", {
@@ -13,4 +46,4 @@
 
     };
     
-}(App.Models, App.DB));
+}(App.Models, App.DB, App.Session, App.Config.get));
